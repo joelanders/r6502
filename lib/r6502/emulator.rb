50e5bc81ea1da4efcadff9c610a9839cc4b6d899
Joe Landers <joe@joelanders.net>
2012-08-11 00:20:33 +0400

    Get some instruction arguments.
    Novelty commit from iPhone at
    Washington Dulles airport.

--- lib/r6502/emulator.rb
+++ lib/r6502/emulator.rb
@@ -4,6 +4,26 @@ module R6502
     def initialize
       @mem = Memory.new
     end
+    def arg(mode, sec_word, thd_word)
+      case mode
+      when :imm
+        sec_word
+      when :zp
+        sec_word
+      when :zpx
+        sec_word + @x
+      when :zpy
+        sec_word + @y
+      when :rel
+        sec_word +@pc
+      when :abs
+        (thd_word<<8) + sec_word
+      when :absx
+        (thd_word<<8) + sec_word + @x
+      when :absy
+        (thd_word<<8) + sec_word + @y
+      end
+    end
 
   end
 end
--- spec/r6502/emulator_spec.rb
+++ spec/r6502/emulator_spec.rb
@@ -31,5 +31,21 @@ module R6502
       @emu.instr_mode(0x65).should == {:instr => :adc, :mode => :zp}
       @emu.instr_mode(0x75).should == {:instr => :adc, :mode => :zpx}
     end
+    it "Finds the argument to the instruction" do
+      @emu = Emulator.new
+      @emu.arg(:imm, 0x40, 0x66).should == 0x40
+      @emu.arg(:zp, 0x50, 0x66).should == 0x50
+      @emu.x = 0xc0
+      @emu.arg(:zpx, 0x0f, 0x66).should == 0xcf
+      @emu.y = 0xb0
+      @emu.arg(:zpy, 0x0f, 0x66).should == 0xbf
+      @emu.pc = 0x100      #normally, pc inc'd during exectn and result
+      @emu.arg(:rel, 0x80, 0x66).should == 0x180  #would be 0x182
+      @emu.arg(:abs, 0xff, 0x66).should == 0x66ff
+      @emu.x = 0x10
+      @emu.arg(:absx, 0xef, 0x66).should == 0x66ff
+      @emu.y = 0x20
+      @emu.arg(:absy, 0xdf, 0x66).should == 0x66ff
+    end
   end
 end
