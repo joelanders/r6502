d00436d5a5ee27dcdf984bd409b3a2778a1b2b4f
Joe Landers <joe@joelanders.net>
2012-11-09 00:58:02 -0800

    Pushed lots of work into instr. methods.

--- lib/r6502/cpu_instructions.rb
+++ lib/r6502/cpu_instructions.rb
@@ -3,70 +3,112 @@ module R6502
     # add with carry
     # DEPENDS ON DECIMAL FLAG
     # TODO
-    def adc(a1, a2)
-      a1 + a2
+    def adc(arg_and_mode)
+      @a = @a + arg_and_mode[:arg]
     end
     # logical and
-    def and(a1, a2)
-      a1 & a2
+    def and(arg_and_mode)
+      @a = @a & arg_and_mode[:arg]
     end
     # shift left
-    def asl(a)
-      a<<1
+    def asl(arg_and_mode)
+      case arg_and_mode[:mode]
+      when :acc
+        @a = @a<<1
+      else
+        @mem.set( arg_and_mode[:arg], @mem.get(arg_and_mode[:arg])<<1 )
+      end
     end
     # logical and (result discarded)
-    def bit(a1, a2)
+    def bit(arg_and_mode)
       a1 & a2
     end
     # decrement (memory)
-    def dec(a)
-      a - 1
+    def dec(arg_and_mode)
+      @mem.set( arg_and_mode[:arg], @mem.get(arg_and_mode[:arg]) - 1 )
     end
     # decrement (x)
-    def dex(a)
-      a - 1
+    def dex(arg_and_mode)
+      @x -= 1
     end
     # decrement (y)
-    def dey(a)
-      a - 1
+    def dey(arg_and_mode)
+      @y -= 1
     end
     # exclusive or
-    def eor(a1, a2)
-      a1 ^ a2
+    def eor(arg_and_mode)
+      case arg_and_mode[:mode]
+      when :imm
+        @a = @a ^ arg_and_mode[:arg]
+      else
+        @a = @a ^ @mem.get(arg_and_mode[:arg])
+      end
     end
     # increment (memory)
-    def inc(a)
-      a + 1
+    def inc(arg_and_mode)
+      @mem.set( arg_and_mode[:arg], @mem.get(arg_and_mode[:arg]) + 1 )
     end
     # increment (x)
-    def inx(a)
-      a + 1
+    def inx(arg_and_mode)
+      @x += 1
     end
     # increment (y)
-    def iny(a)
-      a + 1
+    def iny(arg_and_mode)
+      @y += 1
     end
     # shift right
-    def lsr(a)
-      a>>1
+    def lsr(arg_and_mode)
+      case arg_and_mode[:mode]
+      when :acc
+        @a = @a>>1
+      else
+        @mem.set( arg_and_mode[:arg], @mem.get(arg_and_mode[:arg]) >> 1 )
+      end
     end
     # inclusive or
-    def ora(a1, a2)
-      a1 | a2
+    def ora(arg_and_mode)
+      case arg_and_mode[:mode]
+      when :imm
+        @a = @a | arg_and_mode[:arg]
+      else
+        @a = @a | @mem.get( arg_and_mode[:arg] )
+      end
     end
     # rotate left
-    def rol(a)
-      (0xff & (a<<1)) | ((0x80 & a)>>7)
+    def rol(arg_and_mode)
+      case arg_and_mode[:mode]
+      when :acc
+        @a = (0xff & (@a<<1)) | ((0x80 & @a)>>7)
+      else
+        val = @mem.get(arg_and_mode[:arg])
+        val = (0xff & (val<<1)) | ((0x80 & val)>>7)
+        @mem.set(arg_and_mode[:arg], val)
+      end
     end
     # rotate right
-    def ror(a)
-      (a>>1) | ((0x01 & a)<<7)
+    def ror(arg_and_mode)
+      #(a>>1) | ((0x01 & a)<<7)
+      case arg_and_mode[:mode]
+      when :acc
+        @a = (@a>>1) | ((0x01 & @a)<<7)
+      else
+        val = @mem.get(arg_and_mode[:arg])
+        val = (val>>1) | ((0x01 & val)<<7)
+        @mem.set(arg_and_mode[:arg], val)
+      end
     end
     # subtract with carry
     # DEPENDS ON DECIMAL FLAG
     # TODO
-    def sbc(a1, a2)
-      (0xff & (a1 - a2))
+    def sbc(arg_and_mode)
+      case arg_and_mode[:mode]
+      when :imm
+        val = arg_and_mode[:arg]
+        @a = (0xff & (@a - val))
+      else
+        val = @mem.get( arg_and_mode[:arg] )
+        @a = (0xff & (@a - val))
+      end
     end
     # no operation
     def nop(arg_and_mode)
--- spec/r6502/cpu_instructions_spec.rb
+++ spec/r6502/cpu_instructions_spec.rb
@@ -17,57 +17,152 @@ module R6502
     # DEPENDS ON DECIMAL FLAG
     # TODO
     it "adc" do
-      @cpu.adc(0x01, 0x0f).should == 0x10
+      @cpu.a = 0x02
+      @cpu.adc( {arg: 0x01, mode: :imm} )
+      @cpu.a.should == 0x03
     end
     it "and" do
-      @cpu.and(0xab, 0x0f).should == 0x0b
+      @cpu.a = 0xab
+      @cpu.and( {arg: 0x0f, mode: :imm} )
+      @cpu.a.should == 0x0b
     end
     it "asl" do
-      @cpu.asl(0x01).should == 0x02
+      @cpu.a = 0x01
+      @cpu.asl( {arg: nil, mode: :acc} )
+      @cpu.a.should == 0x02
+      @cpu.asl( {arg: nil, mode: :acc} )
+      @cpu.a.should == 0x04
+
+      @mem.set(0x2010, 0x04)
+      @cpu.asl( {arg: 0x2010, mode: :abs} )
+      @mem.get(0x2010).should == 0x08
+      @cpu.asl( {arg: 0x2010, mode: :abs} )
+      @mem.get(0x2010).should == 0x10
     end
-    it "bit" do
-      @cpu.bit(0x01, 0x02).should == 0x00
+    pending "bit" do
+      #@cpu.bit(0x01, 0x02).should == 0x00
+      @mem.set(0x3010, 0x01)
+      @cpu.a = 0x02
+      @cpu.z = 0
+      @cpu.bit( {arg: 0x3010, mode: :abs} )
+      @cpu.z.should == 1
     end
     it "dec" do
-      @cpu.dec(0x10).should == 0x0f
+      @mem.set(0x4010, 0x02)
+      @cpu.dec( {arg: 0x4010, mode: :abs} )
+      @mem.get(0x4010).should == 0x01
+      @cpu.dec( {arg: 0x4010, mode: :abs} )
+      @mem.get(0x4010).should == 0x00
     end
     it "dex" do
-      @cpu.dex(0x02).should == 0x01
+      @cpu.x = 0x10
+      @cpu.dex( {arg: nil, mode: :imp} )
+      @cpu.x.should == 0x0f
     end
     it "dey" do
-      @cpu.dey(0x03).should == 0x02
+      @cpu.y = 0x0f
+      @cpu.dey( {arg: nil, mode: :imp} )
+      @cpu.y.should == 0x0e
     end
     it "eor" do
-      @cpu.eor(0x05, 0x06).should == 0x03
+      @mem.set(0x1000, 0x05)
+      @cpu.a = 0x06
+      @cpu.eor( {arg: 0x1000, mode: :abs} )
+      @cpu.a.should == 0x03
+
+      @cpu.a = 0x06
+      @cpu.eor( {arg: 0x05, mode: :imm} )
+      @cpu.a.should == 0x03
     end
     it "inc" do
-      @cpu.inc(0x10).should == 0x11
+      @mem.set(0x2000, 0x0f)
+      @cpu.inc( {arg: 0x2000, mode: :abs} )
+      @mem.get(0x2000).should == 0x10
+      @cpu.inc( {arg: 0x2000, mode: :abs} )
+      @mem.get(0x2000).should == 0x11
     end
     it "inx" do
-      @cpu.inx(0x02).should == 0x03
+      @cpu.x = 0x00
+      @cpu.inx( {arg: nil, mode: :imp} )
+      @cpu.x.should == 0x01
+      @cpu.inx( {arg: nil, mode: :imp} )
+      @cpu.x.should == 0x02
     end
     it "iny" do
-      @cpu.iny(0x03).should == 0x04
+      @cpu.y = 0x03
+      @cpu.iny( {arg: nil, mode: :imp} )
+      @cpu.y.should == 0x04
+      @cpu.iny( {arg: nil, mode: :imp} )
+      @cpu.y.should == 0x05
     end
     it "lsr" do
-      @cpu.lsr(0x05).should == 0x02
+      @cpu.a = 0x05
+      @cpu.lsr( {arg: nil, mode: :acc} )
+      @cpu.a.should == 0x02
+      @cpu.lsr( {arg: nil, mode: :acc} )
+      @cpu.a.should == 0x01
+
+      @mem.set(0x1010, 0x07)
+      @cpu.lsr( {arg: 0x1010, mode: :abs} )
+      @mem.get(0x1010).should == 0x03
     end
     it "ora" do
-      @cpu.ora(0x05, 0x06).should == 0x07
+      @cpu.a = 0x05
+      @mem.set(0x1000, 0x06)
+      @cpu.ora( {arg: 0x1000, mode: :abs} )
+      @cpu.a.should == 0x07
+      
+      @cpu.a = 0x05
+      @cpu.ora( {arg: 0x06, mode: :imm} )
+      @cpu.a.should == 0x07
     end
     it "rol" do
-      @cpu.rol(0x80).should == 0x01
-      @cpu.rol(0x01).should == 0x02
-      @cpu.rol(0x81).should == 0x03
-      @cpu.rol(0xff).should == 0xff
+      @cpu.a = 0x80
+      @cpu.rol( {arg: nil, mode: :acc} )
+      @cpu.a.should == 0x01
+      @cpu.rol( {arg: nil, mode: :acc} )
+      @cpu.a.should == 0x02
+
+      @mem.set(0x900, 0x80)
+      @cpu.rol( {arg: 0x900, mode: :abs} )
+      @mem.get(0x900).should == 0x01
+      @cpu.rol( {arg: 0x900, mode: :abs} )
+      @mem.get(0x900).should == 0x02
     end
     it "ror" do
-      @cpu.ror(0x01).should == 0x80
+      @cpu.a = 0x01
+      @cpu.ror( {arg: nil, mode: :acc} )
+      @cpu.a.should == 0x80
+      @cpu.ror( {arg: nil, mode: :acc} )
+      @cpu.a.should == 0x40
+
+      @mem.set(0x900, 0x01)
+      @cpu.ror( {arg: 0x900, mode: :abs} )
+      @mem.get(0x900).should == 0x80
+      @cpu.ror( {arg: 0x900, mode: :abs} )
+      @mem.get(0x900).should == 0x40
+    end
+    it "inx" do
+      @cpu.x = 0x01
+      @cpu.inx( {arg: nil, mode: :imp} )
+      @cpu.x.should == 0x02
+    end
+    it "iny" do
+      @cpu.y = 0x02
+      @cpu.iny( {arg: nil, mode: :imp} )
+      @cpu.y.should == 0x03
     end
     # DEPENDS ON DECIMAL FLAG
     # TODO
     it "sbc" do
-      @cpu.sbc(0x10, 0x0a).should == 0x06
+      @cpu.a = 0x10
+      @mem.set(0x100, 0x0a)
+      @cpu.sbc( {arg: 0x100, mode: :abs} )
+      @cpu.a.should == 0x06
+
+      @cpu.a = 0x10
+      @cpu.sbc( {arg: 0x0a, mode: :imm} )
+      @cpu.a.should == 0x06
     end
     it "nop" do
       @cpu.nop({arg: nil, mode: :imp})
