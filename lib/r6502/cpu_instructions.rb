2e8046aad90ce255cd2a010065d741b852fa23e9
Joe Landers <joe@joelanders.net>
2012-11-09 18:32:08 -0800

    Added all(?) instr.s. Flag-setting not complete.

--- lib/r6502/cpu_instructions.rb
+++ lib/r6502/cpu_instructions.rb
@@ -21,7 +21,12 @@ module R6502
     end
     # logical and (result discarded)
     def bit(arg_and_mode)
-      a1 & a2
+      m = @mem.get( arg_and_mode[:arg] )
+      a = @a
+      result = a & m
+      @z = 1 if result == 0
+      @o = (m & 0x40)>>6
+      @n = (m & 0x80)>>7
     end
     # decrement (memory)
     def dec(arg_and_mode)
@@ -117,9 +122,197 @@ module R6502
     def sec(arg_and_mode)
       @c = 1
     end
+    def sed(arg_and_mode)
+      @d = 1
+    end
+    def sei(arg_and_mode)
+      @i = 1
+    end
     def bcc(arg_and_mode)
       inc_pc_by_mode(:rel)
       @pc += arg_and_mode[:arg] if @c == 0
+    end
+    def bcs(arg_and_mode)
+      inc_pc_by_mode(:rel)
+      @pc += arg_and_mode[:arg] if @c == 1
+    end
+    def beq(arg_and_mode)
+      inc_pc_by_mode(:rel)
+      @pc += arg_and_mode[:arg] if @z == 1
+    end
+    def bmi(arg_and_mode)
+      inc_pc_by_mode(:rel)
+      @pc += arg_and_mode[:arg] if @n == 1
+    end
+    def bne(arg_and_mode)
+      inc_pc_by_mode(:rel)
+      @pc += arg_and_mode[:arg] if @z == 1
+    end
+    def bpl(arg_and_mode)
+      inc_pc_by_mode(:rel)
+      @pc += arg_and_mode[:arg] if @n == 0
+    end
+    def bvc(arg_and_mode)
+      inc_pc_by_mode(:rel)
+      @pc += arg_and_mode[:arg] if @o == 0
+    end
+    def bvs(arg_and_mode)
+      inc_pc_by_mode(:rel)
+      @pc += arg_and_mode[:arg] if @o == 1
+    end
+    def clc()
+      @c = 0
+    end
+    def cld()
+      @d = 0
+    end
+    def cli()
+      @i = 0
+    end
+    def clv()
+      @o = 0
+    end
+    def cmp( arg_and_mode )
+      case arg_and_mode[:mode]
+      when :imm
+        result = @a - arg_and_mode[:arg]
+        @c = result >= 0 ? 1 : 0
+        @z = result == 0 ? 1 : 0
+        #TODO negative flag
+      else
+        m = @mem.get( arg_and_mode[:arg] )
+        result = @a - m
+        @c = result >= 0 ? 1 : 0
+        @z = result == 0 ? 1 : 0
+        #TODO negative flag
+      end
+    end
+    def cpx( arg_and_mode )
+      case arg_and_mode[:mode]
+      when :imm
+        result = @x - arg_and_mode[:arg]
+        @c = result >= 0 ? 1 : 0
+        @z = result == 0 ? 1 : 0
+        #TODO negative flag
+      else
+        m = @mem.get( arg_and_mode[:arg] )
+        result = @x - m
+        @c = result >= 0 ? 1 : 0
+        @z = result == 0 ? 1 : 0
+        #TODO negative flag
+      end
+    end
+    def cpy( arg_and_mode )
+      case arg_and_mode[:mode]
+      when :imm
+        result = @y - arg_and_mode[:arg]
+        @c = result >= 0 ? 1 : 0
+        @z = result == 0 ? 1 : 0
+        #TODO negative flag
+      else
+        m = @mem.get( arg_and_mode[:arg] )
+        result = @y - m
+        @c = result >= 0 ? 1 : 0
+        @z = result == 0 ? 1 : 0
+        #TODO negative flag
+      end
+    end
+    def jmp( arg_and_mode )
+      case arg_and_mode[:mode]
+      when :abs
+        @pc = arg_and_mode[:arg]
+      else #indirect
+        lsb = @mem.get( arg_and_mode[:arg] )
+        msb = @mem.get( arg_and_mode[:arg] + 1)
+        @pc = lsb + msb<<8
+      end
+    end
+    def lda( arg_and_mode )
+      case arg_and_mode[:mode]
+      when :imm
+        @a = arg_and_mode[:arg]
+      else
+        @a = @mem.get( arg_and_mode[:arg] )
+      end
+    end
+    def ldx( arg_and_mode )
+      case arg_and_mode[:mode]
+      when :imm
+        @x = arg_and_mode[:arg]
+      else
+        @x = @mem.get( arg_and_mode[:arg] )
+      end
+    end
+    def ldy( arg_and_mode )
+      case arg_and_mode[:mode]
+      when :imm
+        @y = arg_and_mode[:arg]
+      else
+        @y = @mem.get( arg_and_mode[:arg] )
+      end
+    end
+    def pha()
+      addr = 0x0100 + (0xff & @s)
+      @mem.set( addr, @a )
+      @s -= 1
+    end
+    def pla()
+      addr = 0x0100 + (0xff & (@s + 1))
+      @a = @mem.get( addr )
+      @s += 1
+    end
+    def php()
+      addr = 0x0100 + (0xff & @s)
+      val =            @n #bit 7
+      val = (val<<1) + @o #bit 6
+      val = (val<<1) +  1 #bit 5
+      val = (val<<1) + @b #bit 4
+      val = (val<<1) + @d #bit 3
+      val = (val<<1) + @i #bit 2
+      val = (val<<1) + @z #bit 1
+      val = (val<<1) + @c #bit 0
+
+      @mem.set( addr, val )
+      @s -= 1
+    end
+    def plp()
+      addr = 0x0100 + (0xff & (@s + 1))
+      val = @mem.get( addr )
+      @c = 0x1 & val
+      @z = 0x1 & (val>>1)
+      @i = 0x1 & (val>>2)
+      @d = 0x1 & (val>>3)
+      @b = 0x1 & (val>>4)
+      # bit 5
+      @o = 0x1 & (val>>6)
+      @n = 0x1 & (val>>7)
+    end
+    def sta( arg_and_mode )
+      @mem.set( arg_and_mode[:arg], @a )
+    end
+    def stx( arg_and_mode )
+      @mem.set( arg_and_mode[:arg], @x )
+    end
+    def sty( arg_and_mode )
+      @mem.set( arg_and_mode[:arg], @y )
+    end
+    def tax()
+      @x = @a
+    end
+    def tay()
+      @y = @a
+    end
+    def tsx()
+      @x = @s
+    end
+    def txa()
+      @a = @x
+    end
+    def txs()
+      @s = @x
+    end
+    def tya()
+      @a = @y
     end
   end
 end
--- spec/r6502/cpu_instructions_spec.rb
+++ spec/r6502/cpu_instructions_spec.rb
@@ -39,13 +39,19 @@ module R6502
       @cpu.asl( {arg: 0x2010, mode: :abs} )
       @mem.get(0x2010).should == 0x10
     end
-    pending "bit" do
-      #@cpu.bit(0x01, 0x02).should == 0x00
-      @mem.set(0x3010, 0x01)
+    it "bit" do
+      @mem.set(0x3010, 0xf1)
       @cpu.a = 0x02
       @cpu.z = 0
+      @cpu.o = 0
+      @cpu.n = 0
+
       @cpu.bit( {arg: 0x3010, mode: :abs} )
       @cpu.z.should == 1
+      @cpu.a.should == 0x02
+      @mem.get(0x3010).should == 0xf1
+      @cpu.o.should == 1
+      @cpu.n.should == 1
     end
     it "dec" do
       @mem.set(0x4010, 0x02)
@@ -172,6 +178,16 @@ module R6502
       @cpu.sec({})
       @cpu.c.should == 1
     end
+    it "sed" do
+      @cpu.d = 0
+      @cpu.sed({})
+      @cpu.d.should == 1
+    end
+    it "sei" do
+      @cpu.i = 0
+      @cpu.sei({})
+      @cpu.i.should == 1
+    end
     it "bcc" do
       @cpu.pc = 0x1000
       @cpu.c  = 1
@@ -182,6 +198,353 @@ module R6502
       @cpu.c  = 0
       @cpu.bcc( {arg: 0x10, mode: :rel} )
       @cpu.pc.should == 0x1012
+    end
+    it "bcs" do
+      @cpu.pc = 0x1000
+      @cpu.c  = 0
+      @cpu.bcs( {arg: 0x10, mode: :rel} )
+      @cpu.pc.should == 0x1002
+
+      @cpu.pc = 0x1000
+      @cpu.c  = 1
+      @cpu.bcs( {arg: 0x10, mode: :rel} )
+      @cpu.pc.should == 0x1012
+    end
+    it "beq" do
+      @cpu.pc = 0x1000
+      @cpu.z  = 0
+      @cpu.beq( {arg: 0x10, mode: :rel} )
+      @cpu.pc.should == 0x1002
+
+      @cpu.pc = 0x1000
+      @cpu.z  = 1
+      @cpu.beq( {arg: 0x10, mode: :rel} )
+      @cpu.pc.should == 0x1012
+    end
+    it "bmi" do
+      @cpu.pc = 0x1000
+      @cpu.n  = 0
+      @cpu.bmi( {arg: 0x10, mode: :rel} )
+      @cpu.pc.should == 0x1002
+
+      @cpu.pc = 0x1000
+      @cpu.n  = 1
+      @cpu.bmi( {arg: 0x10, mode: :rel} )
+      @cpu.pc.should == 0x1012
+    end
+    it "bne" do
+      @cpu.pc = 0x1000
+      @cpu.z  = 0
+      @cpu.bne( {arg: 0x10, mode: :rel} )
+      @cpu.pc.should == 0x1002
+
+      @cpu.pc = 0x1000
+      @cpu.z  = 1
+      @cpu.bne( {arg: 0x10, mode: :rel} )
+      @cpu.pc.should == 0x1012
+    end
+    it "bpl" do
+      @cpu.pc = 0x1000
+      @cpu.n  = 1
+      @cpu.bpl( {arg: 0x10, mode: :rel} )
+      @cpu.pc.should == 0x1002
+
+      @cpu.pc = 0x1000
+      @cpu.n  = 0
+      @cpu.bpl( {arg: 0x10, mode: :rel} )
+      @cpu.pc.should == 0x1012
+    end
+    it "bvc" do
+      @cpu.pc = 0x1000
+      @cpu.o  = 1
+      @cpu.bvc( {arg: 0x10, mode: :rel} )
+      @cpu.pc.should == 0x1002
+
+      @cpu.pc = 0x1000
+      @cpu.o  = 0
+      @cpu.bvc( {arg: 0x10, mode: :rel} )
+      @cpu.pc.should == 0x1012
+    end
+    it "bvs" do
+      @cpu.pc = 0x1000
+      @cpu.o  = 0
+      @cpu.bvs( {arg: 0x10, mode: :rel} )
+      @cpu.pc.should == 0x1002
+
+      @cpu.pc = 0x1000
+      @cpu.o  = 1
+      @cpu.bvs( {arg: 0x10, mode: :rel} )
+      @cpu.pc.should == 0x1012
+    end
+    pending "brk" do
+    end
+    it "clc" do
+      @cpu.c = 1
+      @cpu.clc
+      @cpu.c.should == 0
+
+      @cpu.c = 0
+      @cpu.clc
+      @cpu.c.should == 0
+    end
+    it "cld" do
+      @cpu.d = 1
+      @cpu.cld
+      @cpu.d.should == 0
+
+      @cpu.d = 0
+      @cpu.cld
+      @cpu.d.should == 0
+    end
+    it "cli" do
+      @cpu.i = 1
+      @cpu.cli
+      @cpu.i.should == 0
+
+      @cpu.i = 0
+      @cpu.cli
+      @cpu.i.should == 0
+    end
+    it "clv" do
+      @cpu.o = 1
+      @cpu.clv
+      @cpu.o.should == 0
+
+      @cpu.o = 0
+      @cpu.clv
+      @cpu.o.should == 0
+    end
+    #TODO negative flag
+    it "cmp" do
+      @mem.set(0x1000, 0x10)
+      @cpu.a = 0x20
+      @cpu.c = 0
+      @cpu.z = 1
+      @cpu.cmp( {arg: 0x1000, mode: :abs} )
+      @cpu.a.should == 0x20
+      @cpu.c.should == 1
+      @cpu.z.should == 0
+
+      @cpu.a = 0x10
+      @cpu.c = 0
+      @cpu.z = 0
+      @cpu.cmp( {arg: 0x10, mode: :imm} )
+      @cpu.a.should == 0x10
+      @cpu.c.should == 1
+      @cpu.z.should == 1
+    end
+    #TODO negative flag
+    it "cpx" do
+      @mem.set(0x1000, 0x10)
+      @cpu.x = 0x20
+      @cpu.c = 0
+      @cpu.z = 1
+      @cpu.cpx( {arg: 0x1000, mode: :abs} )
+      @cpu.x.should == 0x20
+      @cpu.c.should == 1
+      @cpu.z.should == 0
+
+      @cpu.x = 0x10
+      @cpu.c = 0
+      @cpu.z = 0
+      @cpu.cpx( {arg: 0x10, mode: :imm} )
+      @cpu.x.should == 0x10
+      @cpu.c.should == 1
+      @cpu.z.should == 1
+    end
+    #TODO negative flag
+    it "cpy" do
+      @mem.set(0x1000, 0x10)
+      @cpu.y = 0x20
+      @cpu.c = 0
+      @cpu.z = 1
+      @cpu.cpy( {arg: 0x1000, mode: :abs} )
+      @cpu.y.should == 0x20
+      @cpu.c.should == 1
+      @cpu.z.should == 0
+
+      @cpu.y = 0x10
+      @cpu.c = 0
+      @cpu.z = 0
+      @cpu.cpy( {arg: 0x10, mode: :imm} )
+      @cpu.y.should == 0x10
+      @cpu.c.should == 1
+      @cpu.z.should == 1
+    end
+    it "jmp" do
+      @cpu.pc = 0x1000
+      @cpu.jmp( {arg: 0x2000, mode: :abs} )
+      @cpu.pc.should == 0x2000
+
+      @cpu.pc = 0x1000
+      @mem.set( 0x2000, 0x00 )
+      @mem.set( 0x2001, 0x11 )
+      @cpu.jmp( {arg: 0x2000, mode: :ind} )
+      @cpu.pc.should == 0x1100
+    end
+    pending "jsr" do
+      #need stack push instr first
+    end
+    pending "rts" do
+      #need stack pull instr first
+    end
+    it "lda" do
+      @cpu.a = 0x66
+      @cpu.lda( {arg: 0x33, mode: :imm} )
+      @cpu.a.should == 0x33
+
+      @cpu.a = 0x66
+      @mem.set( 0x1000, 0x44 )
+      @cpu.lda( {arg:0x1000, mode: :abs} )
+      @cpu.a.should == 0x44
+    end
+    it "ldx" do
+      @cpu.x = 0x66
+      @cpu.ldx( {arg: 0x33, mode: :imm} )
+      @cpu.x.should == 0x33
+
+      @cpu.x = 0x66
+      @mem.set( 0x1000, 0x44 )
+      @cpu.ldx( {arg:0x1000, mode: :abs} )
+      @cpu.x.should == 0x44
+    end
+    it "ldy" do
+      @cpu.y = 0x66
+      @cpu.ldy( {arg: 0x33, mode: :imm} )
+      @cpu.y.should == 0x33
+
+      @cpu.y = 0x66
+      @mem.set( 0x1000, 0x44 )
+      @cpu.ldy( {arg:0x1000, mode: :abs} )
+      @cpu.y.should == 0x44
+    end
+    it "pha" do
+      @cpu.a = 0x33
+      @cpu.s = 0xff
+      @cpu.pha()
+      @cpu.s.should == 0xfe
+      @mem.get(0x01ff).should == 0x33
+    end
+    it "pla" do
+      @cpu.a = 0x66
+      @cpu.s = 0xfe
+      @mem.set(0x01ff, 0x34)
+      @cpu.pla()
+      @cpu.a.should == 0x34
+      @cpu.s.should == 0xff
+    end
+    it "php" do
+      @cpu.c = 1
+      @cpu.z = 0
+      @cpu.i = 1
+      @cpu.d = 0
+      @cpu.b = 1
+      #bit 5   1
+      @cpu.o = 0
+      @cpu.n = 1
+
+      @cpu.s = 0xff
+      @cpu.php()
+      @cpu.s.should == 0xfe
+      @mem.get( 0x01ff ).should == 0b10110101
+
+      @cpu.c = 0
+      @cpu.z = 1
+      @cpu.i = 0
+      @cpu.d = 1
+      @cpu.b = 0
+      #bit 5   1
+      @cpu.o = 1
+      @cpu.n = 0
+
+      @cpu.s = 0xfe
+      @cpu.php()
+      @cpu.s.should == 0xfd
+      @mem.get( 0x01fe ).should == 0b01101010
+    end
+    it "plp" do
+      @cpu.c = 1
+      @cpu.z = 0
+      @cpu.i = 1
+      @cpu.d = 0
+      @cpu.b = 1
+      #bit 5   1
+      @cpu.o = 0
+      @cpu.n = 1
+
+      @mem.set(0x01ff, 0b01101010)
+      @cpu.s = 0xfe
+      @cpu.plp()
+
+      @cpu.c.should == 0
+      @cpu.z.should == 1
+      @cpu.i.should == 0
+      @cpu.d.should == 1
+      @cpu.b.should == 0
+      #bit 5           1
+      @cpu.o.should == 1
+      @cpu.n.should == 0
+    end
+    pending "rti"
+    it "sta" do
+      @cpu.a = 0xfa
+      @mem.set( 0x1000, 0xaf )
+      @cpu.sta( {arg: 0x1000, mode: :abs} )
+      @mem.get( 0x1000 ).should == 0xfa
+    end
+    it "stx" do
+      @cpu.x = 0xfa
+      @mem.set( 0x1000, 0xaf )
+      @cpu.stx( {arg: 0x1000, mode: :abs} )
+      @mem.get( 0x1000 ).should == 0xfa
+    end
+    it "sty" do
+      @cpu.y = 0xfa
+      @mem.set( 0x1000, 0xaf )
+      @cpu.sty( {arg: 0x1000, mode: :abs} )
+      @mem.get( 0x1000 ).should == 0xfa
+    end
+    it "tax" do
+      @cpu.x = 0xea
+      @cpu.a = 0xbd
+      @cpu.tax()
+      @cpu.a.should == 0xbd
+      @cpu.x.should == 0xbd
+    end
+    it "tay" do
+      @cpu.y = 0xea
+      @cpu.a = 0xbd
+      @cpu.tay()
+      @cpu.a.should == 0xbd
+      @cpu.y.should == 0xbd
+    end
+    it "tsx" do
+      @cpu.x = 0xea
+      @cpu.s = 0xbd
+      @cpu.tsx()
+      @cpu.s.should == 0xbd
+      @cpu.x.should == 0xbd
+    end
+    it "txa" do
+      @cpu.a = 0xea
+      @cpu.x = 0xbd
+      @cpu.txa()
+      @cpu.x.should == 0xbd
+      @cpu.a.should == 0xbd
+    end
+    it "txs" do
+      @cpu.s = 0xea
+      @cpu.x = 0xbd
+      @cpu.txs()
+      @cpu.x.should == 0xbd
+      @cpu.s.should == 0xbd
+    end
+    it "tya" do
+      @cpu.a = 0xea
+      @cpu.y = 0xbd
+      @cpu.tya()
+      @cpu.y.should == 0xbd
+      @cpu.a.should == 0xbd
     end
   end
 end
